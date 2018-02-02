caf3a3fa49862e5c75a09dd28a4d7f500c79475a
xj <449134105@qq.com>
2018-02-02 15:32:36 +0800

    aaa

--- src/main/java/cn/bos/Main.java
+++ src/main/java/cn/bos/Main.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        System.out.println(111);
         SpringApplication.run(bosApplication.class, args);
     }
 }
